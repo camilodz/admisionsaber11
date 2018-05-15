3893ef552ced763d61d5b00b61b268ee65fd5abf
Karen <karenza@unicauca.edu.co>
2018-05-14 22:52:27 -0500

    Cambio de nombres

---
+++ SistemaAdmisiones/src/java/com/bean/aplicaBean.java
@@ -0,0 +1,77 @@
+package com.bean;
+
+import com.dao.AplicaDao;
+import com.dao.AplicaDaoImp;
+import com.model.Aplica;
+import com.utilidades.Utilidades;
+import java.io.Serializable;
+import java.util.List;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author HP
+ */
+@Named(value = "aplicaBean")
+@ApplicationScoped
+public class aplicaBean implements Serializable{
+    private List<Aplica> listar;
+    private String selectedPro;
+    private String selectedNomPro;
+    private List<Aplica> filtro;
+    
+    public List<Aplica> getListar() {        
+        AplicaDao apdao = new AplicaDaoImp();
+        listar = apdao.mostrarAplica(selectedPro);
+        return listar;
+    }
+
+    public List<Aplica> getFiltro() {
+        return filtro;
+    }
+
+    public void setFiltro(List<Aplica> filtro) {
+        this.filtro = filtro;
+    }
+
+    public Aplica getAplica() {
+        return aplica;
+    }
+
+    public String getSelectedNomPro() {
+        return selectedNomPro;
+    }
+
+    public void setSelectedNomPro(String selectedNomPro) {
+        this.selectedNomPro = selectedNomPro;
+    }
+    
+
+    public void setAplica(Aplica aplica) {
+        this.aplica = aplica;
+    }
+
+    public String getSelectedPro() {
+        return selectedPro;
+    }
+
+    public void setSelectedPro(String selectedPro) {
+        this.selectedPro = selectedPro;
+    }
+    
+    
+    private Aplica aplica;
+    /**
+     * Creates a new instance of AplicaBean
+     */
+    public aplicaBean() {
+    }
+    public void verAdmitidosPublicados(){
+        Utilidades.redireccionar("/SistemaAdmisiones/faces/Vistas/ListadosDeAdmitidos/listarAdmitidosPublicado.xhtml");
+    }
+    public void verAdmitidosPrograma(String selectedPro){
+        this.setSelectedPro(selectedPro);
+        Utilidades.redireccionar("/SistemaAdmisiones/faces/Vistas/ListadosDeAdmitidos/listarAdmitidosPorPrograma.xhtml");
+    }
+}
