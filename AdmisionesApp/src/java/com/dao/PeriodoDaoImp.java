0ee03e2eef178f1526dd82f34212e30159dac905
Karen <karenza@unicauca.edu.co>
2018-06-07 15:48:34 -0500

    Documentación

--- AdmisionesApp/src/java/com/dao/PeriodoDaoImp.java
+++ AdmisionesApp/src/java/com/dao/PeriodoDaoImp.java
@@ -1,11 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.dao;
 
-import com.model.Facultad;
 import com.model.Periodo;
 import com.util.HibernateUtil;
 import java.util.List;
@@ -13,11 +7,19 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 /**
+ * Contiene la implementación de los métodos para gestionar los periodos
+ * académicos
  *
- * @author Karen
+ * @author Proyecto II - Grupo Admisiones
  */
 public class PeriodoDaoImp implements PeriodoDao {
 
+    /**
+     * Implementación del método para obtener (desde la BD) la lista de todos
+     * los periodos académicos
+     *
+     * @return lista de los periodos académicos
+     */
     @Override
     public List<Periodo> mostrarPeriodos() {
         List<Periodo> listaPer = null;
@@ -27,32 +29,38 @@ public class PeriodoDaoImp implements PeriodoDao {
         try {
             listaPer = session.createQuery(hql).list();
             transaction.commit();
-            session.close();            
-        }catch (Exception e){
+            session.close();
+        } catch (Exception e) {
             System.out.println(e.getMessage());
             transaction.rollback();
-        } 
+        }
         return listaPer;
-    }    
+    }
 
+    /**
+     * Declaración del método para agregar (a la BD) un nuevo periodo académico
+     *
+     * @param per nuevo periodo académico que se va a configurar
+     */
     @Override
     public void nuevoPeriodo(Periodo per) {
-     
+
         Session session = null;
-        try{
-            
-        session = HibernateUtil.getSessionFactory().openSession();
-        Transaction transaction = session.beginTransaction();
-        session.save(per);
-        session.getTransaction().commit();
+        try {
+
+            session = HibernateUtil.getSessionFactory().openSession();
+            Transaction transaction = session.beginTransaction();
+            session.save(per);
+            session.getTransaction().commit();
             System.out.println(".....");
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
             session.getTransaction().rollback();
-        }finally{
-            if(session != null)
+        } finally {
+            if (session != null) {
                 session.close();
-        } 
+            }
+        }
     }
-    
+
 }
