d9e1c9ff24fbe348dd426fda269da2ff6cf759c6
Karen <karenza@unicauca.edu.co>
2018-06-07 15:46:30 -0500

    Documentación

--- AdmisionesApp/src/java/com/dao/programaofertadoDao.java
+++ AdmisionesApp/src/java/com/dao/programaofertadoDao.java
@@ -1,21 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.dao;
 
-import com.model.Facultad;
 import com.model.Programaofertado;
 import java.util.List;
 
 /**
+ * Contiene la declaración de los métodos para gestionar los programas ofertados
+ * en un periodo académico Obtener y editar los programas ofertados desde la BD
  *
- * @author Karen
+ * @author Proyecto II - Grupo Admisiones
  */
 public interface programaofertadoDao {
-    // Método para mostrar lista de programas ofertados
+
+    /**
+     * Declaración del método para obtener (desde la BD) la lista de los
+     * programas ofertados de todas las facultades
+     *
+     * @return lista de los programas ofertados de todas las facultades
+     */
     public List<Programaofertado> listarPO();
+
+    /**
+     * Declaración del método para modificar los cupos y ponderados ICFES de un
+     * programa ofertado específico
+     *
+     * @param po programa ofertado que se va a modificar
+     */
     public void modificarPO(Programaofertado po);
+
+    /**
+     * Declaración del método para listar los programas ofertados de una
+     * facultad específica
+     *
+     * @param facid id de la facultad de la cual queremos obtener los programas
+     * ofertados
+     * @return lista de los programas ofertados de la facultad específica
+     */
     public List<Programaofertado> listarPOFacultad(String facid);
 }
