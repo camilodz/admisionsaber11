30ead5bd4a5caa41a69c415b034aed46920bd4dd
Karen <karenza@unicauca.edu.co>
2018-06-07 15:47:32 -0500

    Documentación

--- AdmisionesApp/src/java/com/dao/facultadDao.java
+++ AdmisionesApp/src/java/com/dao/facultadDao.java
@@ -1,18 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.dao;
 
 import com.model.Facultad;
 import java.util.List;
 
 /**
+ * Contiene la declaración de los métodos para gestionar las facultades
  *
- * @author Karen
+ * @author Proyecto II - Grupo Admisiones
  */
 public interface facultadDao {
+
+    /**
+     * Declaración del método para obtener (desde la BD) la lista de las
+     * facultades
+     *
+     * @return lista de las facultades
+     */
     public List<Facultad> listarFac();
+
+    /**
+     * Declaración del método para obtener (desde la BD) el id de una facultad
+     * dado su nombre
+     *
+     * @param nombreFac nombre de la facultad de la cual se quiere obtener su id
+     * @return id de la facultad específica
+     */
     public String getIdFacultad(String nombreFac);
 }
