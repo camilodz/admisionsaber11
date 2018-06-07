428f0df0acf55c017e24ebb029a2770c3883ba1a
Karen <karenza@unicauca.edu.co>
2018-06-07 15:49:59 -0500

    Documentación

--- AdmisionesApp/src/java/com/dao/AplicaDaoImp.java
+++ AdmisionesApp/src/java/com/dao/AplicaDaoImp.java
@@ -1,7 +1,6 @@
 package com.dao;
 
 import com.model.Aplica;
-import com.model.Aspirante;
 import com.model.AspiranteNoPonderable;
 import com.model.AspirantePonderable;
 import com.model.Resultadoicfes;
@@ -12,17 +11,27 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 /**
+ * Contiene la implementación de los métodos para gestionar la información de un
+ * aspirante PONDERABLE y NO PONDERABLE que aplica a un un programa específico
  *
- * @author HP
+ * @author Proyecto II - Grupo Admisiones
  */
-public class AplicaDaoImp implements AplicaDao{
+public class AplicaDaoImp implements AplicaDao {
 
+    /**
+     * Implementación del método para listar (desde la BD) los aspirantes de un
+     * programa específico
+     *
+     * @param selectedPro id del programa del cual se van a listar los
+     * aspirantes
+     * @return lista de aspirantes del pro
+     */
     @Override
     public List<Aplica> mostrarAplica(String selectedPro) {
         List<Aplica> listAplica = null;
         Session session = HibernateUtil.getSessionFactory().openSession();
         Transaction transaction = session.beginTransaction();
-        String hql = "FROM Aplica as a inner join fetch a.programaofertado inner join fetch a.aspirante Where proid ="+ selectedPro;
+        String hql = "FROM Aplica as a inner join fetch a.programaofertado inner join fetch a.aspirante Where proid =" + selectedPro;
         try {
             listAplica = session.createQuery(hql).list();
             transaction.commit();
@@ -33,14 +42,20 @@ public class AplicaDaoImp implements AplicaDao{
         }
         return listAplica;
     }
-    
-     @Override
+
+    /**
+     * Implementación del método para listar (desde la BD) todos los aspirantes
+     * a los programas ofertados
+     *
+     * @return lista de los aspirantes de todos los programas
+     */
+    @Override
     public List<Aplica> mostrarAspirantes() {
         List<Aplica> listAspirantes = null;
         Session session = HibernateUtil.getSessionFactory().openSession();
         Transaction transaction = session.beginTransaction();
         String hql = "from Aplica as a inner join fetch a.aspirante";
-        
+
         try {
             listAspirantes = session.createQuery(hql).list();
             transaction.commit();
@@ -51,7 +66,13 @@ public class AplicaDaoImp implements AplicaDao{
         }
         return listAspirantes;
     }
-    
+
+    /**
+     * Implementación del método para listar (desde la BD) los resultados ICFES
+     * de todos los aspirantes
+     *
+     * @return lista de los resultados ICFES de todos los aspirantes
+     */
     @Override
     public List<Resultadoicfes> listarResultados() {
         List<Resultadoicfes> listaR = null;
@@ -61,123 +82,148 @@ public class AplicaDaoImp implements AplicaDao{
         try {
             listaR = session.createQuery(hql).list();
             transaction.commit();
-            session.close();            
-        }catch (Exception e){
+            session.close();
+        } catch (Exception e) {
             System.out.println(e.getMessage());
             transaction.rollback();
-        } 
+        }
         return listaR;
     }
 
+    /**
+     * Implementación del método para agregar (a la BD) un aspirante a la lista
+     * de PONDERABLES (podría ser admitido)
+     *
+     * @param a aspirante ponderable (información del aspirante + los resultados
+     * ICFES)
+     */
     @Override
     public void insertPonderables(AspirantePonderable a) {
-        
-         Session session = null;
-        try{
+
+        Session session = null;
+        try {
             session = HibernateUtil.getSessionFactory().openSession();
             session.beginTransaction();
-            session.save(a);     
-            
+            session.save(a);
+
             session.getTransaction().commit();
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
-            
+
             session.getTransaction().rollback();
-            
-        }finally{
-            if(session!=null){
+
+        } finally {
+            if (session != null) {
                 session.close();
             }
         }
     }
 
+    /**
+     * Implementación del método para agregar (a la BD) un aspirante a la lista
+     * de NO PONDERABLES (bajo ninguna circunstancia será admitido)
+     *
+     * @param a aspirante ponderable (información del aspirante + el motivo por
+     * el cual no se pudo ponderar)
+     */
     @Override
     public void insertNoPonderables(AspiranteNoPonderable a) {
-        
-         Session session = null;
-        try{
-            
+
+        Session session = null;
+        try {
+
             session = HibernateUtil.getSessionFactory().openSession();
             session.beginTransaction();
             session.save(a);
             session.getTransaction().commit();
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
             session.getTransaction().rollback();
-            
-        }finally{
-            if(session!=null){
+
+        } finally {
+            if (session != null) {
                 session.close();
             }
         }
     }
 
-    
-    // Listar aspirantes ponderables y no ponderables por programa
+    /**
+     * Implementación del método para eliminar (desde la BD) un aspirante de la
+     * lista de NO PONDERABLES (Para poder reintegrarlo a ponderables)
+     *
+     * @param a aspirante no ponderable que se va a pasar a la lista de
+     * ponderables
+     */
+    @Override
+    public void eliminarNoPonderable(AspiranteNoPonderable a) {
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.delete(a);
+
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+
+            session.getTransaction().rollback();
+
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    /**
+     * Implementación del método para listar (desde la BD) los aspirantes
+     * PONDERABLES y NO PONDERABLES de un programa específico
+     *
+     * @param proid programa del cual se van a listar los aspirantes
+     * @return lista de los aspirantes de un programa específico
+     */
     @Override
     public List<Aplica> listarAspirantes(String proid) {
         System.out.println("********************************* " + proid);
         List<Aplica> listaA = null;
         Session session = HibernateUtil.getSessionFactory().openSession();
         Transaction transaction = session.beginTransaction();
-        String hql = "FROM Aplica as a inner join fetch a.programaofertado inner join fetch a.aspirante where proid = " +proid;
+        String hql = "FROM Aplica as a inner join fetch a.programaofertado inner join fetch a.aspirante where proid = " + proid;
         try {
             listaA = session.createQuery(hql).list();
             transaction.commit();
-            session.close();            
-        }catch (Exception e){
+            session.close();
+        } catch (Exception e) {
             System.out.println(e.getMessage());
             transaction.rollback();
-        } 
-        return listaA;    
-        
-        
-        //FROM Aplica as a inner join fetch a.programaofertado inner join fetch a.aspirante
+        }
+        return listaA;
     }
-    
-     @Override
+
+    /**
+     * Implementación del método para obtener (desde la BD) el número de
+     * aspirantes de cada programa
+     *
+     * @return lista de la cantidad de aspirantes de cada programa
+     */
+    @Override
     public List<Integer> numAspirantes() {
-        
-        
+
         Session session = HibernateUtil.getSessionFactory().openSession();
         Transaction transaction = session.beginTransaction();
         List<Integer> listaAspirantes = new ArrayList<>();
-        
+
         try {
-            
+
             listaAspirantes = session.createQuery("select count(*) FROM Aplica as a inner join fetch a.programaofertado inner join fetch a.aspirante group by(proid)").list();
-            
-            session.close();    
-            
-        }catch (Exception e){
+
+            session.close();
+
+        } catch (Exception e) {
             System.out.println(e.getMessage());
             transaction.rollback();
-        }   
+        }
         return listaAspirantes;
     }
 
-    @Override
-    public void eliminarNoPonderable(AspiranteNoPonderable a) {
-        Session session = null;
-        try{
-            session = HibernateUtil.getSessionFactory().openSession();
-            session.beginTransaction();
-            session.delete(a);
-            
-            session.getTransaction().commit();
-        }catch(Exception e){
-            System.out.println(e.getMessage());
-            
-            session.getTransaction().rollback();
-            
-        }finally{
-            if(session!=null){
-                session.close();
-            }
-        }
-    }
-
-    
-    
-    
 }
