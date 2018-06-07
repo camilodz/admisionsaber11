26656e0964dddc6e780f54650076408dd3304d42
Karen <karenza@unicauca.edu.co>
2018-06-07 15:49:38 -0500

    Documentación

--- AdmisionesApp/src/java/com/dao/AspiranteDao.java
+++ AdmisionesApp/src/java/com/dao/AspiranteDao.java
@@ -4,10 +4,19 @@ import com.model.Aspirante;
 import java.util.List;
 
 /**
+ * Contiene la declaración de los métodos para gestionar los aspirantes a todos
+ * programas
  *
- * @author HP
+ * @author Proyecto II - Grupo Admisiones
  */
 public interface AspiranteDao {
+
+    /**
+     * Declaración del método para listar (desde la BD) los aspirantes a todos
+     * los programas
+     *
+     * @return lista de aspirantes de todos los programas
+     */
     public List<Aspirante> mostrarAspirantes();
-    
+
 }
