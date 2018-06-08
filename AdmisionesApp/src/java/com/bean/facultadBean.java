aefa700010b5972d25d790c7d9fedbc3f92b81a0
Karen <karenza@unicauca.edu.co>
2018-06-07 22:06:53 -0500

    Documentación

--- AdmisionesApp/src/java/com/bean/facultadBean.java
+++ AdmisionesApp/src/java/com/bean/facultadBean.java
@@ -3,7 +3,6 @@ package com.bean;
 import com.dao.facultadDao;
 import com.dao.facultadDaoImp;
 import com.model.Facultad;
-import com.model.Programaofertado;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,21 +11,44 @@ import javax.enterprise.context.RequestScoped;
 import javax.faces.model.SelectItem;
 
 /**
+ * Bean facultadBean
  *
- * @author Karen
+ * Contiene los métodos para gestionar las facultades
+ *
+ * @author Proyecto II - Grupo Admisiones
  */
 @Named(value = "facultadBean")
 @RequestScoped
 public class facultadBean implements Serializable {
 
+    /*** Atributos ***/ 
+    
+    /**
+     * Lista de las facultades
+     */
     private List<Facultad> listaFac;
+    
+    /**
+     * Facultad de la cual se van a obtener los programas ofertados
+     */
     private Facultad fac;
     
+    /**
+     * Id de la facultad
+     */
     private Short selectedFac;  
     
+    /*** Constructor ***/
+    
+    /**
+     * Constructor por defecto
+     */
     public facultadBean() {
     }
 
+    
+    /*** Métodos getter y setter ***/
+    
     public Facultad getFac() {
         return fac;
     }
@@ -35,14 +57,6 @@ public class facultadBean implements Serializable {
         this.fac = fac;
     }
     
-    
-    
-    public List<Facultad> getListaFac() {
-        facultadDao facDao = new facultadDaoImp();
-        this.listaFac = facDao.listarFac();
-        return listaFac;
-    }
-
     public Short getSelectedFac() {
         return selectedFac;
     }
@@ -51,6 +65,25 @@ public class facultadBean implements Serializable {
         this.selectedFac = selectedFac;
     }
     
+    
+    /*** Métodos públicos ***/
+    
+    /**
+     * Obtener la lista de todas las facultades 
+     * 
+     * @return lista de todas las facultades
+     */
+    public List<Facultad> getListaFac() {
+        facultadDao facDao = new facultadDaoImp();
+        this.listaFac = facDao.listarFac();
+        return listaFac;
+    }
+
+    /**
+     * Obtener la lista de las facultades para mostrar en el combobox
+     * 
+     * @return lista de las facultades
+     */
     public List<SelectItem> facultades(){
         List<SelectItem> listaPro = new ArrayList<>();
         this.getListaFac();
