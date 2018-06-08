06324362aa61014c36a5bbe3eccebc9319d6c72a
Karen <karenza@unicauca.edu.co>
2018-06-07 20:49:44 -0500

    Documentación

--- AdmisionesApp/src/java/com/dao/facultadDao.java
+++ AdmisionesApp/src/java/com/dao/facultadDao.java
@@ -4,12 +4,16 @@ import com.model.Facultad;
 import java.util.List;
 
 /**
+ * Interface facultadDao
+ * 
  * Contiene la declaración de los métodos para gestionar las facultades
  *
  * @author Proyecto II - Grupo Admisiones
  */
 public interface facultadDao {
 
+    /*** Declaración de los métodos ***/
+    
     /**
      * Declaración del método para obtener (desde la BD) la lista de las
      * facultades
@@ -23,7 +27,7 @@ public interface facultadDao {
      * dado su nombre
      *
      * @param nombreFac nombre de la facultad de la cual se quiere obtener su id
-     * @return id de la facultad específica
+     * @return id de la facultad específicada
      */
     public String getIdFacultad(String nombreFac);
 }
